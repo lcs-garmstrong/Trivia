a6a82a76d4fa8ea08e79b67476bd96575c393fe4
Graeme Armstrong <garmstrong@lcs.on.ca>
2023-04-19 12:04:32 -0400

    Initial Commit

---
+++ Trivia/TriviaApp.swift
@@ -0,0 +1,17 @@
+//
+//  TriviaApp.swift
+//  Trivia
+//
+//  Created by Graeme Armstrong on 2023-04-19.
+//
+
+import SwiftUI
+
+@main
+struct TriviaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
