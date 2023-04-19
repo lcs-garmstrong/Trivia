43d293f48db1c515a9d8ec1b61c2cbc3f3187685
Graeme Armstrong <garmstrong@lcs.on.ca>
2023-04-19 12:13:46 -0400

    Created project and completed initial organization of views.

---
+++ Trivia/TriviaView.swift
@@ -0,0 +1,20 @@
+//
+//  TriviaView.swift
+//  Trivia
+//
+//  Created by Graeme Armstrong on 2023-04-19.
+//
+
+import SwiftUI
+
+struct TriviaView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TriviaView_Previews: PreviewProvider {
+    static var previews: some View {
+        TriviaView()
+    }
+}
