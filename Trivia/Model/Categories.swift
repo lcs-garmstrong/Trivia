8e44b10f15b27a8ad066d019c8a8e1fc73f589b9
Graeme Armstrong <garmstrong@lcs.on.ca>
2023-04-20 13:05:05 -0400

    Added more categories to possible categories

--- Trivia/Model/Categories.swift
+++ Trivia/Model/Categories.swift
@@ -20,4 +20,18 @@ let film = CategoryDescription(categoryName: "Entertainment: Film", categoryId: 
 
 let music = CategoryDescription(categoryName: "Entertainment: Music", categoryId: 12)
 
-let allCategories = [generalKnowledge, books, film, music]
+let science = CategoryDescription(categoryName: "Science: Mathematics", categoryId: 19)
+
+let sports = CategoryDescription(categoryName: "Sports", categoryId: 21)
+
+let geography = CategoryDescription(categoryName: "Geography", categoryId: 22)
+
+let history = CategoryDescription(categoryName: "History", categoryId: 23)
+
+let celebrities = CategoryDescription(categoryName: "Celebrities", categoryId: 26)
+
+let animals = CategoryDescription(categoryName: "Animals", categoryId: 27)
+
+let vehicles = CategoryDescription(categoryName: "Vehicles", categoryId: 28)
+
+let allCategories = [generalKnowledge, books, film, music, science, sports, geography, history, celebrities, animals, vehicles]
