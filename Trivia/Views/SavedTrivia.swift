e53e3101d3ea5aedcb101febd6b6188d3c446f2a
Graeme Armstrong <garmstrong@lcs.on.ca>
2023-04-22 20:23:58 -0400

    Text commit

--- Trivia/Views/SavedTrivia.swift
+++ Trivia/Views/SavedTrivia.swift
@@ -10,68 +10,27 @@ import SwiftUI
 
 
 struct SavedTrivia: View {
-    
     // MARK: Stored properties
+    
     // for searchbar
     @State var searchText = ""
-    
-    @BlackbirdLiveModels({ db in
-        try await TriviaQuestion.read(from: db,
-        sqlWhere: "category LIKE ?", "%\(searchText)")
-    }) var savedTrivia
     
     // use to talk to DB
     @Environment(\.blackbirdDatabase) var db: Blackbird.Database?
     
     var body: some View {
         NavigationView{
-            List{
+            VStack{
                 
-                ForEach(savedTrivia.results) { currentTrivia in
-                    VStack(alignment: .leading) {
-                        
-                        HStack{
-                            Text("Category:")
-                                .bold()
-                            Text(currentTrivia.category)
-                        }
-                        Spacer()
-                        Text(currentTrivia.question)
-                            .font(.title3)
-                            .bold()
-                        
-                        Text(currentTrivia.correct_answer)
-                            .font(.title3)
-                        
-                    }
-                    .padding(5)
-                }
-                .onDelete(perform: removeRows)
+                Text("Search Category")
+                
+                SavedTriviaItemView(filteredOn: searchText)
+                    .searchable(text: $searchText)
             }
-            .searchable(text: $searchText)
             .navigationTitle("Saved Trivia:")
         }
     }
     
-    // MARK: Functions:
-    func removeRows(at offsets: IndexSet){
-        Task{
-            try await db!.transaction { core in
-                var idList = ""
-                for offset in offsets {
-                    idList += "\(savedTrivia.results[offset].id),"
-                }
-                
-                // romove final coma
-                print(idList)
-                idList.removeLast()
-                print(idList)
-                
-                try core.query("DELETE FROM TriviaQuestion WHERE id IN (?)", idList)
-            }
-        }
-
-    }
 }
 
 
@@ -79,6 +38,5 @@ struct SavedTrivia_Previews: PreviewProvider {
     static var previews: some View {
         SavedTrivia()
             .environment(\.blackbirdDatabase, AppDatabase.instance)
-        
     }
 }
--- Trivia/Views/SavedTriviaItemView.swift
+++ Trivia/Views/SavedTriviaItemView.swift
@@ -5,16 +5,80 @@
 //  Created by Graeme Armstrong on 2023-04-20.
 //
 
+import Blackbird
 import SwiftUI
 
 struct SavedTriviaItemView: View {
+    // MARK: Stored properties
+    
+    // use to talk to DB
+    @Environment(\.blackbirdDatabase) var db: Blackbird.Database?
+    
+    @BlackbirdLiveModels var savedTrivia: Blackbird.LiveResults<TriviaQuestion>
+    
+    // MARK: Computed properties
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            List{
+                
+                ForEach(savedTrivia.results) { currentTrivia in
+                    VStack(alignment: .leading) {
+                        
+                        HStack{
+                            Text("Category:")
+                                .bold()
+                            Text(currentTrivia.category)
+                        }
+                        Spacer()
+                        Text(currentTrivia.question)
+                            .font(.title3)
+                            .bold()
+                        
+                        Text(currentTrivia.correct_answer)
+                            .font(.title3)
+                        
+                    }
+                    .padding(5)
+                }
+                .onDelete(perform: removeRows)
+                
+            }
+    }
+    
+    // MARK: Initializer(s)
+    init(filteredOn searchText: String) {
+        
+        // initialize live model
+        _savedTrivia = BlackbirdLiveModels({ db in
+            try await TriviaQuestion.read(from: db,
+            sqlWhere: "category LIKE ?", "%\(searchText)%")
+        })
+    }
+    
+    
+    // MARK: Functions:
+    func removeRows(at offsets: IndexSet){
+        Task{
+            try await db!.transaction { core in
+                var idList = ""
+                for offset in offsets {
+                    idList += "\(savedTrivia.results[offset].id),"
+                }
+                
+                // romove final coma
+                print(idList)
+                idList.removeLast()
+                print(idList)
+                
+                try core.query("DELETE FROM TriviaQuestion WHERE id IN (?)", idList)
+            }
+        }
     }
 }
 
 struct SavedTriviaItemView_Previews: PreviewProvider {
     static var previews: some View {
-        SavedTriviaItemView()
+        SavedTriviaItemView(filteredOn: "")
+        // pull from database
+            .environment(\.blackbirdDatabase, AppDatabase.instance)
     }
 }
